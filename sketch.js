3ebb0be7342f39c2933a17873cba1b611a226a62
Art2B <arthur.battut@gmail.com>
2018-02-10 21:09:10 +0100

    handle file upload to apply geometry to it

--- sketch.js
+++ sketch.js
@@ -1,6 +1,76 @@
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
+const handleFile = () => {
+  const x = document.getElementById('fr-upload')
+  if (x.files && x.files[0]) {
+    const fr = new FileReader();
+    fr.onload = e => {
+      const img = new Image()
+      img.addEventListener('load', () => {
+        canvas.width = img.width
+        canvas.height = img.height
+        addFilters()
+        ctx.drawImage(img, 0, 0)
+        const diagMaxRadius = 100
+        drawRecursiveDiamondsAllOverCanvas(canvas, diagMaxRadius)
+      })
+      img.src = e.target.result
+    }
+    fr.readAsDataURL(x.files[0])
+  }
+}
+
+const addFilters = () => {
+  ctx.filter = 'grayscale(100%) brightness(120%)'
+}
+
+const drawLines = (canvas, img) => {
+  const imgData = getImageData(img)
+  const context = canvas.getContext('2d')
+
+  imgData.forEach((pixel, index) => {
+    if (Math.floor(index/img.width) % 2 === 0) {
+      const xPos = index % img.width
+      const yPos = Math.floor(index/img.width)
+      ctx.fillStyle = `rgb(${pixel.r}, ${pixel.g}, ${pixel.b})`
+      ctx.fillRect( xPos, yPos, 1,1)
+    }
+  })
+
+  // for (let i = 0; i < canvas.height; i = i+4) {
+  //   context.beginPath()
+  //   context.moveTo(0,i)
+  //   context.lineTo(canvas.width, i)
+  //   canvas.lineWidth = 1
+  //   context.stroke()
+  // }
+}
+
+const getImageData = img => {
+  const canvas = document.createElement('canvas')
+  canvas.width = img.width
+  canvas.height = img.height
+
+  const ctx = canvas.getContext('2d')
+  ctx.drawImage(img, 0, 0)
+  const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+
+  // Make human readable data
+  let data = []
+
+  for (let i=0; i < imgData.data.length; i = i+4) {
+    let pixel = {
+      r: imgData.data[i],
+      g: imgData.data[i + 1],
+      b: imgData.data[i + 2],
+      a: imgData.data[i + 3]
+    }
+    data.push(pixel)
+  }
+  return data
+}
+
 /**
  * drawDiamond
  * Draw a diamon shape from top point and diamon length
@@ -32,6 +102,7 @@ const drawDiamond = (topPoint, diagonal) => {
   ctx.lineTo(bottomPoint.x, bottomPoint.y)
   ctx.lineTo(leftPoint.x, leftPoint.y)
   ctx.closePath()
+  ctx.strokeStyle = "#FFFFFF"
   ctx.stroke()
 }
 
@@ -46,9 +117,6 @@ const drawDiamond = (topPoint, diagonal) => {
  */
 const drawRecursiveDiamonds = (center, radius, outEdges = true) => {
   const step = 4
-  // Draw center
-  ctx.fillStyle = 'black'
-  ctx.fillRect(center.x - 1, center.y - 1, 2, 2)
   // Draw diamonds
   const loopLimit = (outEdges) ? radius : radius - step
   for (let i=0; i <= loopLimit; i += step) {
@@ -67,7 +135,7 @@ const drawRecursiveDiamonds = (center, radius, outEdges = true) => {
  * @param  {number} radius - The radius of diamonds
  */
 const drawRecursiveDiamondsAllOverCanvas = (canvas, radius) => {
-  for (let y = 0; y <= canvas.height; y += radius) {
+  for (let y = 0; y <= canvas.height + radius; y += radius) {
     if ((y/radius) % 2 === 0) {
       for (let x = 0; x < (canvas.width / radius*2); x++) {
         const center = {
@@ -90,4 +158,4 @@ const drawRecursiveDiamondsAllOverCanvas = (canvas, radius) => {
 
 // SCRIPTS BEGIN HEEEEEEEERE
 const diagMaxRadius = 100
-drawRecursiveDiamondsAllOverCanvas(canvas, diagMaxRadius)
+// drawRecursiveDiamondsAllOverCanvas(canvas, diagMaxRadius)
