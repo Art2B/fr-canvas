d2a4ccbd0be03fc6da6261ea9b41593552386a97
Art2B <arthur.battut@gmail.com>
2018-02-11 22:15:30 +0100

    Display uploaded image in canvas

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,17 +1,50 @@
-import { h, app } from "hyperapp"
+import 'babel-polyfill'
+import { h, app } from 'hyperapp'
 
-const state = {}
-
-const actions = {}
-
-const handleFile = event => {
-  console.log(event.target.files[0])
+const state = {
+  image: ''
 }
+
+const actions = {
+  handleFile: file => async (state, actions) => {
+    const img = await new Promise(resolve => {
+      const fr = new FileReader()
+      fr.onload = e => {
+        resolve(e.target.result)
+      }
+      fr.readAsDataURL(file)
+    })
+    actions.setImage(img)
+  },
+  setImage: value => state => ({ image: value })
+}
+
+const drawImage = (canvas, base64) => {
+  const img = new Image()
+  const ctx = canvas.getContext('2d')
+
+  img.addEventListener('load', () => {
+    // Update canvas size to match img size
+    canvas.width = img.width
+    canvas.height = img.height
+    // Draw image on canvas
+    ctx.drawImage(img, 0, 0)
+  })
+  img.src = base64
+}
+
+const Canvas = ({ image }) => (
+  <canvas 
+    onupdate={(element, oldProps) => {
+      drawImage(element, image)
+    }} 
+  />
+)
 
 const view = (state, actions) => (
   <div>
-    <input type="file" id="fr-upload" multiple size="50" onchange={e => handleFile(e)} />
-    <canvas id="canvas"></canvas>
+    <input type="file" id="fr-upload" multiple size="50" onchange={e => actions.handleFile(e.target.files[0])} />
+    <Canvas image={ state.image } />
   </div>
 )
 
