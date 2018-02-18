6fb7bf98aa86e5d26c3d686352c86cd591c1a43b
Art2B <arthur.battut@gmail.com>
2018-02-18 13:23:51 +0100

    Import components in app

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,8 @@
 import 'babel-polyfill'
 import { h, app } from 'hyperapp'
 
-import Hexagon from './hexagon'
+import Canvas from './components/canvas'
+import App from './components/app'
 
 const state = {
   image: ''
@@ -21,37 +22,4 @@ const actions = {
   setImage: value => state => ({ image: value })
 }
 
-const drawImage = (canvas, base64) => {
-  const img = new Image()
-  const ctx = canvas.getContext('2d')
-
-  img.addEventListener('load', () => {
-    // Update canvas size to match img size
-    canvas.width = img.width
-    canvas.height = img.height
-    // Draw image on canvas
-    ctx.filter = 'grayscale(100%)'
-    ctx.drawImage(img, 0, 0)
-    // Draw hexagons on image
-    const hexa = new Hexagon(canvas, 100)
-    hexa.draw()
-  })
-  img.src = base64
-}
-
-const Canvas = ({ image }) => (
-  <canvas
-    onupdate={(element, oldProps) => {
-      drawImage(element, image)
-    }}
-  />
-)
-
-const view = (state, actions) => (
-  <div>
-    <input type='file' id='fr-upload' multiple size='50' onchange={e => actions.handleFile(e.target.files[0])} />
-    <Canvas image={state.image} />
-  </div>
-)
-
-app(state, actions, view, document.body)
+app(state, actions, App, document.body)
