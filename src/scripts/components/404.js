5a76cbd6c8f79b9ae4e77f15c6aee6adc21e73b6
Art2B <arthur.battut@gmail.com>
2018-02-27 11:59:49 +0100

    Add 404 component

---
+++ src/scripts/components/404.js
@@ -0,0 +1,10 @@
+import { h, app } from 'hyperapp'
+import { Link } from "@hyperapp/router"
+
+export default () => (
+  <section className='not-found'>
+    <h2>404 not found</h2>
+    <p>I can't find the page you're looking for.</p>
+    <p>So shut up ang go <Link to='/'>home</Link></p>
+  </section>
+)
