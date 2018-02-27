4e7804317b5495faaa2ea931f091d215413f3315
Art2B <arthur.battut@gmail.com>
2018-02-27 15:11:57 +0100

    Display error message

--- src/scripts/components/home/index.js
+++ src/scripts/components/home/index.js
@@ -2,15 +2,20 @@ import { h, app } from 'hyperapp'
 
 import Canvas from './canvas'
 
-export default ({image, handleFile}) => () => (
-  <div>
+export default ({image, error, handleFile}) => () => (
+  <main>
     <section className='explanations'>
       <p>Choose an image, let me process it, and BAM! You have a cooler image made with hexagons.</p>
     </section>
     <section className={image ? 'home is-file' : 'home'}>
-      <label className='file-input' for='fr-upload'><span className='text-bg'>Choose a <i A className="fas fa-fire" /> af image.</span></label>
-      <input type='file' id='fr-upload' size='50' onchange={e => handleFile(e.target.files[0])} />
+      <div className='input-container'>
+        <label for='fr-upload'><span className='text-bg'>Choose a <i A className="fas fa-fire" /> af image.</span></label>
+        <input type='file' id='fr-upload' size='50' onchange={e => handleFile(e.target.files[0])} />
+        { error &&
+          <p class='file-error'>{error}</p>
+        }
+      </div>
       <Canvas image={image} />
     </section>
-  </div>
+  </main>
 )
