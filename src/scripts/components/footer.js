0fe61168705e75792eccf48c413517c360c11c95
Art2B <arthur.battut@gmail.com>
2018-02-27 15:21:48 +0100

    Deploy

--- src/scripts/components/footer.js
+++
@@ -1,8 +0,0 @@
-import { h, app } from 'hyperapp'
-
-export default ({}) => (
-  <footer>
-    <div className='border'/>
-    Made with <i className='far fa-keyboard' /> by <a href='https://twitter.com/mr_marabout' target='_blank'>Art2B</a>
-  </footer>
-)
